f595ad9a842f824fd478909f412a7cb1f464a9ae
jamieberrier <berrier.jamie@gmail.com>
2020-07-17 11:48:30 -0700

    create private post_params and create action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::PostsController < ApplicationController
     render json: posts
   end
 
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post, status: :accepted
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:breed_id, :num_of_likes, :picture)
+  end
 end

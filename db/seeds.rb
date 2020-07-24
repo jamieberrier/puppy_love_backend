379c6b9e4d832fb82c0078f02741baef89ee2c13
jamieberrier <berrier.jamie@gmail.com>
2020-07-24 08:59:23 -0700

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,9 @@ Post.destroy_all
 boxer = Breed.create(name: 'Boxer')
 dane = Breed.create(name: 'Great Dane')
 frenchie = Breed.create(name: 'French Bulldog')
+terrier = Breed.create(name: 'Terrier')
 
+Post.create(breed_id: terrier.id, num_of_likes: 79, picture: 'https://www.dogbreedinfo.com/images31/NorfolkTerrierPurebredDogIvy2YearsOld2.jpg')
 Post.create(breed_id: frenchie.id, num_of_likes: 100, picture: 'https://happyfrenchbulldog.com/wp-content/uploads/2019/08/French-bulldog-puppy-guide.jpg')
 Post.create(breed_id: boxer.id, num_of_likes: 105, picture: 'https://www.thesprucepets.com/thmb/DOytfIaKRXrfa-otJlCwDn7EptM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Boxer-GettyImages-463043655-91a77226f5884b41915d50811e4e4e2b.jpg')
 Post.create(breed_id: dane.id, num_of_likes: 89, picture: 'https://www.petful.com/wp-content/uploads/2012/01/great-dane-1.jpg')

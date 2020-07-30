fdd87c9cfdd602babcc3768d143a317e202aa6cd
jamieberrier <berrier.jamie@gmail.com>
2020-07-30 13:31:49 -0700

    create show route for breeds

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: %i[index create update]
-      resources :breeds, only: %i[index]
+      resources :breeds, only: %i[index show]
     end
   end
 end

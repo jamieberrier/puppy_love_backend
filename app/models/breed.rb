f66652ed49672cabba3c1e14ea5617adb92abb91
jamieberrier <berrier.jamie@gmail.com>
2020-08-05 11:50:57 -0700

    create class method request_token to get token from petfinder api

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,23 @@
 class Breed < ApplicationRecord
   has_many :posts, dependent: :destroy
+
+  PETFINDER_API_KEY = ENV['petfinder_api_key']
+  PETFINDER_API_SECRET = ENV['petfinder_api_secret']
+
+  def self.request_token
+    url = URI('https://api.petfinder.com/v2/oauth2/token')
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    request['content-type'] = 'application/x-www-form-urlencoded'
+    request.body = "grant_type=client_credentials&client_id=#{PETFINDER_API_KEY}&client_secret=#{PETFINDER_API_SECRET}"
+
+    response = http.request(request)
+    j_resp = JSON.parse response.read_body
+
+    { token_type: j_resp['token_type'], token: j_resp['access_token'] }
+  end
 end

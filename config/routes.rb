2a49de4abc3570ace4d39354bbbd45c9939a9306
jamieberrier <berrier.jamie@gmail.com>
2020-08-18 13:17:59 -0700

    delete unused breed show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: %i[index create update]
-      resources :breeds, only: %i[index show]
+      resources :breeds, only: %i[index]
       get '/petfinder' => 'petfinder#token'
     end
   end
